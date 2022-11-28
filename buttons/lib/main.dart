ffa7487b1dca8417fb5f75814c31a8536e559f17
badhon424 <badhon424@gmail.com>
2022-11-28 07:53:46 +0600

    button basic

---
+++ buttons/lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomeSection());
+  }
+}
+
+class HomeSection extends StatelessWidget {
+  const HomeSection({Key? key}) : super(key: key);
+
+  mySnackBar(msg, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(msg)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+        padding: EdgeInsets.all(20),
+        primary: Colors.purple,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("APP BAR"),
+        titleSpacing: 100,
+        toolbarHeight: 60,
+        toolbarOpacity: .80,
+        elevation: 5,
+        backgroundColor: Colors.amber,
+        actions: [
+          IconButton(
+              onPressed: () {
+                mySnackBar("Comment", context);
+              },
+              icon: const Icon(Icons.comment)),
+          IconButton(
+              onPressed: () {
+                mySnackBar("Search", context);
+              },
+              icon: const Icon(Icons.search)),
+          IconButton(
+              onPressed: () {
+                mySnackBar("Settings", context);
+              },
+              icon: const Icon(Icons.settings)),
+          IconButton(
+              onPressed: () {
+                mySnackBar("Email", context);
+              },
+              icon: const Icon(Icons.email))
+        ],
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          TextButton(
+            onPressed: () {
+              mySnackBar("This is a text button", context);
+            },
+            child: Text('Text Button'),
+          ),
+          OutlinedButton(
+            onPressed: () {
+              mySnackBar("This is an outlined button", context);
+            },
+            child: Text('Outlined Button'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              mySnackBar("This is an elevated button", context);
+            },
+            child: Text('Elevated Button'),
+            style: buttonStyle,
+          ),
+        ],
+      ),
+    );
+  }
+}
